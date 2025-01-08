43cd9b3c4bab17eb2c577638dedb00c55d9b713a
agastya221 <agastyash213@gmail.com>
2025-01-08 19:30:34 +0530

    Implement PubsubManager class with user subscription and unsubscription methods

--- src/PubSubManager.ts
+++ src/PubSubManager.ts
@@ -1,16 +1,17 @@
 import { createClient, RedisClientType } from "redis";
 
-class PubsubManager {
+export class PubsubManager {
     private static instance: PubsubManager;
     private redisClient: RedisClientType;
-    private Subscriber = Map<string, string[]>
+    private Subscriptions: Map<string, string[]>
 
     private constructor() {
         this.redisClient  = createClient();
         this.redisClient.connect();
-        this.Subscriber = new Map();
+        this.Subscriptions = new Map();
         
     }
+
 
     public static getInstance(): PubsubManager{
         if(!PubsubManager.instance){
@@ -19,6 +20,45 @@ class PubsubManager {
         return PubsubManager.instance;
 
     }
+    
+    public userSubscribe(userId:string,  stock: string){
+        if(this.Subscriptions.has(stock)){
+            this.Subscriptions.set(stock, [])
+        }
+        this.Subscriptions.get(stock)?.push(userId);
+
+        if(this.Subscriptions.get(stock)?.length === 1){
+            this.redisClient.subscribe(stock, (message) => {
+                this.handleMessage(stock, message);
+            });
+        } 
+        console.log(`Subscribed to Redis channel: ${stock}`);
+    }
+
+    public userUnsubscribe(userId: string, stock: string){
+        this.Subscriptions.set(stock, this.Subscriptions.get(stock)?.filter((sub)=> sub  !== userId) || []);
+        if(this.Subscriptions.get(stock)?.length === 0){
+            this.redisClient.unsubscribe(stock);
+            console.log(`Unsubscribed from Redis channel: ${stock}`);
+        }
+   
+    }
+
+    private handleMessage(stock: string, message: string){
+        console.log(`Message received on channel ${stock}: ${message}`);
+        this.Subscriptions.get(stock)?.forEach((sub)=>{
+            console.log(`Sending message to user ${sub}`);
+        })
+    }
 
 
+    public async disconnect(){
+        await this.redisClient.quit();
+    }
+
+    
+
+
+
+    
 }
